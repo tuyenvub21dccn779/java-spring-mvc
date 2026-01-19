6cc10bf7596f6ade66b0986a23f059745a22e941
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-19 21:36:56 +0700

    video 61 Định nghĩa Repository Query

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
@@ -22,6 +24,8 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
+        List<User> arrUsers = this.userService.getAllUsersByEmail("5@gmail.com");
+        System.out.println(arrUsers);
         model.addAttribute("eric", "eric");
         model.addAttribute("hoidanit", "from controller with model");
         return "hello";
