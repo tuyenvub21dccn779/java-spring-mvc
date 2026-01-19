2cda039f17a78b84b0c96d0591932d8c775d8a61
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-19 08:53:37 +0700

    video 43

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,7 +8,17 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World with Hỏi Dân It!";
+        return "Hello World!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page";
     }
 
 }
