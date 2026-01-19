4df2d2d09981839b744a930171218647614cbf1e
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-19 17:17:12 +0700

    video 45

---
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -0,0 +1,12 @@
+package vn.hoidanit.laptopshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public String handleHello() {
+        return "hellow from controller";
+    }
+
+}
