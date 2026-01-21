716613276635ba2a1e4c181a6a286d048f3ef197
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-21 16:46:22 +0700

    video 98 Hoàn thiện layout Client

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller()
-public class ProductPageController {
+public class ItemController {
 
     @GetMapping("/product/{id}")
     public String getMethodName(@PathVariable long id) {
