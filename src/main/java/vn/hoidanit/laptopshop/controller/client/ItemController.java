dee43043f158c1c288fcb830d5319b6dd68efc8c
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-22 11:18:10 +0700

    video 106 Hoàn Thiện Xem Chi Tiết Product

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -1,15 +1,28 @@
 package vn.hoidanit.laptopshop.controller.client;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
 
 @Controller()
 public class ItemController {
 
+    private final ProductService productService;
+
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/product/{id}")
-    public String getMethodName(@PathVariable long id) {
-        return "client/product/show";
+    public String getMethodName(Model model, @PathVariable long id) {
+        Product product = productService.getProductById(id).get();
+        model.addAttribute("product", product);
+        model.addAttribute("id", id);
+        return "client/product/detail";
     }
 
 }
